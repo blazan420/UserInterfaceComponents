6ad6d1d75178895048e6a11b6d891c4a8c85becd
kevin <kevinjobrien420@gmail.com>
2018-06-23 03:56:07 -0500

    245 App Number 32 - Part1 - UserInterfaceComponents// Coding

--- app/src/main/java/com/example/kevin/userinterfacecomponents/MainActivity.java
+++ app/src/main/java/com/example/kevin/userinterfacecomponents/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.kevin.userinterfacecomponents;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView txtValue;
+    EditText edtValue;
+    AutoCompleteTextView autoCompleteTextView;
+    MultiAutoCompleteTextView multiAutoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        txtValue = (TextView) findViewById(R.id.txtValue);
+        edtValue = (EditText) findViewById(R.id.edtValue);
+        autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        //multiAutoCompleteTextView = (multiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView);
+
+
+        txtValue.setText("Awesome!");
+        txtValue.setBackgroundColor(Color.BLUE);
+
+        txtValue.setOnClickListener(MainActivity.this);
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        txtValue.setTextColor(Color.RED);
     }
 }
