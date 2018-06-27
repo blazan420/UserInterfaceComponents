c6eeab32e91271ce44cadb9b2c716a1ea247c5cc
kevin <kevinjobrien420@gmail.com>
2018-06-27 02:28:47 -0500

    246 App Number 32 - Part2 - User Interface Components// Complete

--- app/src/main/java/com/example/kevin/userinterfacecomponents/MainActivity.java
+++ app/src/main/java/com/example/kevin/userinterfacecomponents/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
@@ -16,6 +17,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     AutoCompleteTextView autoCompleteTextView;
     MultiAutoCompleteTextView multiAutoCompleteTextView;
 
+    String[] courseNames = {"Android Developer Course", "iOS Developer Course", "Java Developer Course"};
+    ArrayAdapter<String> myAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +29,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         txtValue = (TextView) findViewById(R.id.txtValue);
         edtValue = (EditText) findViewById(R.id.edtValue);
         autoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
-        //multiAutoCompleteTextView = (multiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView);
+        multiAutoCompleteTextView = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView);
 
 
         txtValue.setText("Awesome!");
         txtValue.setBackgroundColor(Color.BLUE);
 
         txtValue.setOnClickListener(MainActivity.this);
+        edtValue.setOnClickListener(MainActivity.this);
+
+        myAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, courseNames);
+        autoCompleteTextView.setAdapter(myAdapter);
+
+
+        multiAutoCompleteTextView.setAdapter(myAdapter);
+        multiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
 
 
 
@@ -40,6 +52,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        txtValue.setTextColor(Color.RED);
+
+        switch (view.getId()){
+
+            case R.id.txtValue:
+                txtValue.setTextColor(Color.RED);
+                break;
+            case R.id.edtValue:
+                txtValue.setText(edtValue.getText());
+                break;
+        }
+
     }
 }
